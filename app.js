8b551c3dc91dc9704462b5b04cac0ec26b02fff9
Manish Kumar <mimanish54@gmail.com>
2023-05-21 11:49:48 +0530

    commit

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const kebabCase = require('lodash/kebabCase');
 
-const homeStartingContent = `Hey!
-Welcome to our daily blog website.
-So be in touch Daily...`
 const aboutContent = `We are developers of this web blog and lets build it together. If any issues are faced then kindly contact us...`
 const contactContent = "Phone no : +91-9523338938 || Email id : mimanish54@gmail.com";
 
 const app = express();
 var posts = [];
+var user;
 
 app.set('view engine', 'ejs');
 
@@ -21,21 +19,29 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res) {
   res.render("home", {
-    homeStartingContent: homeStartingContent,
+    user: user,
     posts: posts
   });
 });
 
 app.get("/about", function(req, res) {
   res.render("about", {
-    aboutContent: aboutContent
+    aboutContent: aboutContent,
+    contactContent: contactContent
   });
 });
 
-app.get("/contact", function(req, res) {
-  res.render("contact", {
-    contactContent: contactContent
-  });
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.post("/login", function(req, res) {
+  const username = {
+    username: req.body.username
+  };
+  user=username.username;
+  res.redirect("/");
+  
 });
 
 app.get("/compose", function(req, res) {
@@ -44,19 +50,20 @@ app.get("/compose", function(req, res) {
 
 app.get("/posts/:postTitle", function(req, res) {
   var requestedTitle = kebabCase(req.params.postTitle);
-
+  var c=0;
   posts.forEach(function(post) {
     var postTitle = kebabCase(post.title);
-    if (postTitle == requestedTitle) {
+    if (postTitle === requestedTitle) {
+      c=1;
       res.render("post", {
         postTitle : post.title,
         postBody : post.body
       });
     }
-    else{
-      res.render("error");
-    }
   });
+  if (c==0){
+    res.render("error");
+  }
 });
 
 app.post("/compose", function(req, res) {
